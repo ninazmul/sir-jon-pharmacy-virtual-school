e7051a444996af373afc0582ee7b7f818e8b0e67
ninazmul <nazmulsaw@gmail.com>
2026-05-03 09:29:50 +0600

    refactor: restructure Home component to use Suspense for loading state

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,28 @@
 import HomeClient from "@/components/shared/HomeClient";
+import Loader from "@/components/shared/Loader";
 import { getCourses } from "@/lib/actions/course.actions";
 import { getSetting } from "@/lib/actions/setting.actions";
+import { Suspense } from "react";
 
 export const revalidate = 60;
 
-export default async function Home() {
+async function HomeContent() {
   const setting = await getSetting();
   const courses = await getCourses({ status: "active" });
 
   return <HomeClient setting={setting} courses={courses} />;
+}
+
+export default function Home() {
+  return (
+    <Suspense
+      fallback={
+        <Loader
+          label="Loading Home Page..."
+        />
+      }
+    >
+      <HomeContent />
+    </Suspense>
+  );
 }
